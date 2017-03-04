1798d3cee872a3946d6ea840b94cdb7dfaa632bd
Domagoj <domagoj.blazonija@gmail.com>
2017-03-04 01:12:33 +0100

    Added CSRF protection to all routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php 
 
+session_start();
+
 $app = new \Slim\App([
     'settings' => [
         'displayErrorDetails' => true, 
@@ -12,6 +14,11 @@ $container['db'] = function () {
     return new PDO('mysql:host=localhost;dbname=slim', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 };
 
+$container['csrf'] = function ($c) {
+    return new \Slim\Csrf\Guard;
+};
+
+
 $container['view'] = function ($container) {
     $view = new \Slim\Views\Twig(__DIR__ . '/../resources/views', [
         'cache' => false
@@ -20,8 +27,11 @@ $container['view'] = function ($container) {
     // Instantiate and add Slim specific extension
     $basePath = rtrim(str_ireplace('index.php', '', $container['request']->getUri()->getBasePath()), '/');
     $view->addExtension(new Slim\Views\TwigExtension($container['router'], $basePath));
+    $view->addExtension(new App\Views\CsrfExtension($container['csrf']));
 
     return $view;
 };
+
+$app->add($container->get('csrf'));
 
 require __DIR__ . '/../routes/web.php';
